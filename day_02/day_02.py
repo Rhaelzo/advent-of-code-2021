86bc0165635a1a0f2e1da22021af19f1f97c3163
Rhaelzo <rhaelzo@outlook.pt>
2021-12-02 11:42:45 +0000

    Second part of day 02 of the advent of code 2021 solved

--- day_02/day_02.py
+++ day_02/day_02.py
@@ -34,6 +34,33 @@ After following these instructions, you would have a horizontal position of 15 a
 
 Calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
 
+--- Part Two ---
+
+Based on your calculations, the planned course doesn't seem to make any sense. You find the submarine manual and discover that the process is actually slightly more complicated.
+
+In addition to horizontal position and depth, you'll also need to track a third value, aim, which also starts at 0. The commands also mean something entirely different than you first thought:
+
+    down X increases your aim by X units.
+    up X decreases your aim by X units.
+    forward X does two things:
+        It increases your horizontal position by X units.
+        It increases your depth by your aim multiplied by X.
+
+Again note that since you're on a submarine, down and up do the opposite of what you might expect: "down" means aiming in the positive direction.
+
+Now, the above example does something different:
+
+    forward 5 adds 5 to your horizontal position, a total of 5. Because your aim is 0, your depth does not change.
+    down 5 adds 5 to your aim, resulting in a value of 5.
+    forward 8 adds 8 to your horizontal position, a total of 13. Because your aim is 5, your depth increases by 8*5=40.
+    up 3 decreases your aim by 3, resulting in a value of 2.
+    down 8 adds 8 to your aim, resulting in a value of 10.
+    forward 2 adds 2 to your horizontal position, a total of 15. Because your aim is 10, your depth increases by 2*10=20 to a total of 60.
+
+After following these new instructions, you would have a horizontal position of 15 and a depth of 60. (Multiplying these produces 900.)
+
+Using this new interpretation of the commands, calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
+
 '''
 
 from io import TextIOWrapper
@@ -55,11 +82,19 @@ def getPlannedCourse(loadedFile: TextIOWrapper):
         course.append([splitLine[0], int(splitLine[1])])
     return course
 
-def goThroughCourse(possibleMovements: dict, plannedCourse: list):
+def goThroughCourse(possibleMovements: dict, plannedCourse: list) -> int:
     finalPosition = vector(0,0,0)
     for courseNode in plannedCourse:
         finalPosition += (possibleMovements[courseNode[0]] * courseNode[1])
-    return finalPosition
+    return int(finalPosition.x * finalPosition.y)
+
+def goThroughCourseWithAim(possibleMovementsWithAim: dict, plannedCourse: list) -> int:
+    finalPosition = vector(0,0,0)
+    for courseNode in plannedCourse:
+        finalPosition += (possibleMovementsWithAim[courseNode[0]] * courseNode[1])
+        if(courseNode[0] == "forward"):
+            finalPosition.y += (finalPosition.z * courseNode[1])
+    return int(finalPosition.x * finalPosition.y)
 
 def main() -> int:
     loadedFile = getFile("input")
@@ -71,9 +106,19 @@ def main() -> int:
         "down": vector(0,1,0)
     }
     course = getPlannedCourse(loadedFile)
-    finalPosition = goThroughCourse(possibleMovements, course)
-    print(str((int)(finalPosition.x * finalPosition.y)))
+    multOne = goThroughCourse(possibleMovements, course)
+    print(str(multOne))
     # -- end Part 1 --
+
+    # -- start Part 2 --
+    possibleMovementsWithAim = {
+        "forward": vector(1,0,0),
+        "up": vector(0,0,-1),
+        "down": vector(0,0,1)
+    }
+    multTwo = goThroughCourseWithAim(possibleMovementsWithAim, course)
+    print(str(multTwo))
+    # -- end Part 2 --
 
     closeFile(loadedFile)
     return 0
